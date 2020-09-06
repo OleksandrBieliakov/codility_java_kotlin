6ad258682aebb035ba26fd7079e71fef0096cf5e
Oleksandr Bieliakov <abel9k@gmail.com>
2020-09-06 12:59:07 +0200

    Lesson 6 finished

--- src/lesson6/Lesson6.kt
+++ src/lesson6/Lesson6.kt
@@ -1,7 +1,6 @@
 package lesson6
 
 import java.lang.Integer.max
-import java.lang.reflect.Array
 
 fun solutionDistinct(A: IntArray): Int {
     return HashSet<Int>(A.asList()).size
@@ -14,7 +13,7 @@ fun solutionTripletProduct(A: IntArray): Int {
     val bottomTwo = sorted.slice(0..1)
     val topTwo = sorted.slice(size - 2 until size)
 
-    val multiply = {acc: Int, i: Int -> acc * i}
+    val multiply = { acc: Int, i: Int -> acc * i }
 
     val topTwoProduct = topTwo.reduce(multiply)
     val bottomTwoProduct = bottomTwo.reduce(multiply)
@@ -29,6 +28,47 @@ fun solutionTripletProduct(A: IntArray): Int {
 }
 
 fun main() {
-    val arr = intArrayOf(-5, 5, -5, 4)
-    println(solutionTripletProduct(arr))
+
+}
+
+fun isTriangle(first: Int, second: Int, third: Int): Boolean {
+    return first >= 0 && first > third - second && second > first - third && first > second - third
+}
+
+fun solutionTriangle(A: IntArray): Int {
+    if (A.size < 3)
+        return 0
+    A.sort()
+    for (i in 0 until A.size - 2)
+        if (isTriangle(A[i], A[i + 1], A[i + 2]))
+            return 1
+    return 0
+}
+
+class Disc(center: Int, radius: Int) : Comparable<Disc> {
+    private val leftEdge: Int = center - radius
+    private val rightEdge: Int = center + radius
+
+    override fun compareTo(other: Disc): Int {
+        return this.leftEdge - other.leftEdge
+    }
+
+    fun intersects(other: Disc): Boolean {
+        return this.rightEdge >= other.leftEdge
+    }
+}
+
+fun solutionDiscs(A: IntArray): Int {
+    val discs = A.mapIndexed { center, radius -> Disc(center, radius) }.sorted()
+    var intersections = 0
+    for (i in 0 until discs.size - 1)
+        for (j in i + 1 until discs.size) {
+            if (discs[i].intersects(discs[j])) {
+                intersections++
+                if (intersections > 10_000_000)
+                    return -1
+            } else
+                break
+        }
+    return intersections
 }
